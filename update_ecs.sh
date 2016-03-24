2ea24dc4b01fc9b4f9344015c4c820af5b9927c2
yukofeb <yuko.february@gmail.com>
2016-03-24 13:35:30 -0400

    update script

--- update_ecs.sh
+++ update_ecs.sh
@@ -3,12 +3,11 @@
 MYSECURITYGROUP=sg-34fc6d51
 MYIP=`curl -s inet-ip.info`
 
-aws ec2 authorize-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
-
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 aws configure set region $AWS_REGION
 
+aws ec2 authorize-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
 aws ecs list-container-instances --cluster hubot
 
 aws ec2 revoke-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
