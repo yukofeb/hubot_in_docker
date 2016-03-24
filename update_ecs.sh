19819a8cf7a625e219544b4dacd1d26bd20104fc
yukofeb <yuko.february@gmail.com>
2016-03-24 13:15:07 -0400

    add authorize-security-group-ingress command

--- update_ecs.sh
+++ update_ecs.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-echo "Start to update Amazon ecs."
+MYSECURITYGROUP=sg-34fc6d51
+MYIP=`curl -s inet-ip.info`
+
+aws ec2 authorize-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
 
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 aws configure set default.region $AWS_REGION
 
 aws ecs list-container-instances --cluster hubot
+
+aws ec2 revoke-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
