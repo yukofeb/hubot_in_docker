9478eec077f0b861b084f7c516491984153b168c
yukofeb <yuko.february@gmail.com>
2016-03-24 11:33:41 -0400

    add test script

--- update_ecs.sh
+++ update_ecs.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 echo "Start to update Amazon ecs."
+
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+aws configure set default.region $AWS_REGION
+
+aws ecs list-container-instances --cluster hubot
