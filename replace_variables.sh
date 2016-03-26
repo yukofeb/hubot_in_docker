62d256bf25c0c244223f5385b2a50424399c52f2
yukofeb <yuko.february@gmail.com>
2016-03-25 21:31:16 -0400

    temporarily modify

--- replace_variables.sh
+++ replace_variables.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Replace _***_ with $***(environment variables)
+# Replace --***-- with $***(environment variables)
 ORIGINAL_FILE=$1
 REPLACED_FILE=$2
 
 cp ./$ORIGINAL_FILE ./$REPLACED_FILE
 cat $REPLACED_FILE | while read line
 do
-  if [[ $line =~ __([A-Z_]+)__ ]] ; then
+  if [[ $line =~ --([A-Z_]+)-- ]] ; then
     KEY=${BASH_REMATCH[1]}
     eval VALUE='$'${KEY}
     sed -i -e "s!${BASH_REMATCH[0]}!$VALUE!g" $REPLACED_FILE
